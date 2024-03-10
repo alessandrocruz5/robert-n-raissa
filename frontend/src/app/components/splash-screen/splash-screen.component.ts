387fde7bf98f6b7094e572b0375b1da298d104ae
alessandrocruz5 <alessandrorafaelcruz@gmail.com>
2024-03-10 14:38:58 +0800

    Responsive image slider

--- frontend/src/app/components/splash-screen/splash-screen.component.ts
+++ frontend/src/app/components/splash-screen/splash-screen.component.ts
@@ -1,20 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  transition,
+  animate,
+  style,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-splash-screen',
   templateUrl: './splash-screen.component.html',
   styleUrls: ['./splash-screen.component.css'],
+  animations: [
+    trigger('fade', [
+      state(
+        'visible',
+        style({
+          opacity: 1,
+        })
+      ),
+      state(
+        'hidden',
+        style({
+          opacity: 0,
+        })
+      ),
+      transition('visible => hidden', animate('1s')),
+    ]),
+  ],
 })
 export class SplashScreenComponent implements OnInit {
   windowWidth: string;
   showSplash: boolean = true;
+  fadeState = 'visible';
+
+  // ngOnInit(): void {
+  //   setTimeout(() => {
+  //     this.windowWidth = '-' + window.innerWidth + 'px';
+  //     setTimeout(() => {
+  //       this.showSplash = !this.showSplash;
+  //     }, 1000);
+  //   }, 1500);
+  // }
 
   ngOnInit(): void {
     setTimeout(() => {
-      this.windowWidth = '-' + window.innerWidth + 'px';
-      setTimeout(() => {
-        this.showSplash = !this.showSplash;
-      }, 1000);
-    }, 1500);
+      this.fadeState = 'hidden';
+    }, 2000);
   }
 }
