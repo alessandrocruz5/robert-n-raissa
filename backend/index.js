ad060eb6b0db5f2b482247fbe1721d85350d6fd7
alessandrocruz5 <alessandrorafaelcruz@gmail.com>
2024-03-12 13:37:30 +0800

    Created .env

--- backend/index.js
+++ backend/index.js
@@ -4,15 +4,15 @@ const Schema = mongoose.Schema;
 const router = express.Router();
 const cors = require("cors");
 const multer = require("multer");
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const CONNECTION_STRING =
-  "mongodb+srv://acruz:tdoUm1ijs9XvBbfT@cluster0.a7izvec.mongodb.net/robert-and-raissa?retryWrites=true&w=majority";
-const PORT = process.env.PORT || 1217;
+const CONNECTION_STRING = process.env.CONNECTION_STRING;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
